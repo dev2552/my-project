925c7ca98af59407a0095e08a1440781facc4cfa
dev2552 <zakariae_aboud@outlook.com>
2019-05-29 16:12:04 +0100

    @

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+	/**
+	* @Route("/")
+	*/
+	public function home(){
+		return $this->render("home/index.html.twig");
+	}
+}
